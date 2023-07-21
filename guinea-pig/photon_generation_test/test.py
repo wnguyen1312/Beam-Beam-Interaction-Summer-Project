ce3f83edc29ab1f12d69b4ccafe20b03a5bef033
William Nguyen <108578700+wnguyen1312@users.noreply.github.com>
2023-07-21 13:04:40 -0700

    testing script updated
    testing script updated for photon_generation_test

--- guinea-pig/photon_generation_test/test.py
+++ guinea-pig/photon_generation_test/test.py
@@ -1,47 +1,16 @@
 # -*- coding: utf-8 -*-
 """
+
+File to test photon generation function works correctly in Python
+
 Created on Thu Jul 20 12:46:26 2023
 
-@author: willi
+@author: william 
 """
 
 import random
 import photon_generator
 import matplotlib.pyplot as plt
-import numpy as np
-
-upsilonSingleP = 0.03  # Fixed upsilon value
-eng = 125  # Fixed particle energy in GeV
-Radius = 17  # Fixed radius
-
-num_particles = 1000000  # Number of particles to test
-dzOnRadius_values = np.logspace(-8, -6, num=50)  # Varying dzOnRadius between 1e-8 and 1e-6
-
-probabilities = []
-
-for dzOnRadius in dzOnRadius_values:
-    photon_count = 0
-
-    for _ in range(num_particles):
-        result = photon_generator.synrad_0_no_spin_flip(upsilonSingleP, eng, dzOnRadius)
-        
-        if result == 1:
-            photon_count += 1
-    
-    probability = photon_count / num_particles
-    probabilities.append(probability)
-
-# Plotting the results
-plt.plot(dzOnRadius_values, probabilities, marker='o')
-plt.xscale('log')
-plt.xlabel('dzOnRadius')
-plt.ylabel('Probability of Photon Generation')
-plt.title('Effect of dzOnRadius on Probability of Photon Generation')
-plt.grid(True)
-plt.show()
-
-#%%
-import photon_generator
 import numpy as np
 
 # Read upsilonSingleP values from 'upsilon_table.txt'
@@ -95,13 +64,18 @@ import matplotlib.pyplot as plt
 
 # Load data from 'photon_test.dat'
 photon_data = np.loadtxt('photon_test.dat')
+photon_data_GP = np.loadtxt('photon(10k macro case).dat')
 
 # Number of data points
 N_photons = len(photon_data)
-print(N_photons)
-#%%
+N_photons_GP = len(photon_data_GP)
+
+print('The number of photons from Python:', N_photons)
+print('The number of photons from GP:', N_photons_GP)
+#%% Plotting
+
 # Set up the histogram parameters
-bin_no = 500
+bin_no = 100
 dE = (np.max(photon_data) - np.min(photon_data)) / bin_no
 
 # Create the figure and axes objects
@@ -110,7 +84,7 @@ fig, ax = plt.subplots(1, 2, figsize=(12, 6))
 # Plot the histogram of photon energies
 # Plot 1/N dN/dE
 H, bins = np.histogram(np.abs(photon_data), bins=bin_no)
-ax[0].plot(bins[:-1], H / N_photons / dE, label='Photon Data')
+ax[0].plot(bins[:-1], H / N_photons / dE, label='Photon Data from Python')
 ax[0].set_xlabel('E [GeV]', fontsize=14)
 ax[0].set_xscale('log')
 ax[0].set_yscale('log')
@@ -121,7 +95,7 @@ ax[0].grid()
 
 # Plot 1/N d(E*N)/dE
 H_weighted, bins_weighted = np.histogram(np.abs(photon_data), bins=bin_no, weights=np.abs(photon_data))
-ax[1].plot(bins_weighted[:-1], H_weighted / N_photons / dE, label='Photon Data')
+ax[1].plot(bins_weighted[:-1], H_weighted / N_photons / dE, label='Photon Data from Python')
 ax[1].set_xlabel('E [GeV]', fontsize=14)
 ax[1].set_xlim(0, 55)
 ax[1].set_title('1/N d(E*N)/dE photons', fontsize=14)
